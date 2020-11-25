0c38f3ee0de58879660b56154d45c392eeb0df32
Karnveer Grewal <karnveergrewal4@gmail.com>
2020-11-25 08:48:37 -0500

    finished project

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-$('.thumbnail').on('click', e => {
-  console.log(e.target);
-});
+function handleThumbnailClicks() {
+  $(".thumbnail").on("click", (e) => {
+    console.log(e.target);
+    const imgSrc = $(e.currentTarget).find("img").attr("src");
+    const imgAlt = $(e.currentTarget).find("img").attr("alt");
+    $(".hero img").attr("src", imgSrc).attr("alt", imgAlt);
+  });
+}
+
+$(handleThumbnailClicks);
